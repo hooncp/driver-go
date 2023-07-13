980752c3eaa459d99edcc5fc128d98ff978279c9
Chee Ping <cheeping.hoon@bytedance.com>
2023-07-13 08:57:19 +0800

    Add in us-region to driver

--- conn/region.go
+++ conn/region.go
@@ -8,11 +8,13 @@ import (
 const (
 	RegionCnNorth1     = "CN-NORTH-1"
 	RegionApSouthEast1 = "AP-SOUTHEAST-1"
+	RegionUsEast1      = "US-EAST-1"
 )
 
 var hostPortByRegion = map[string]string{
 	RegionCnNorth1:     "gateway.aws-cn-north-1.bytehouse.cn:19000",
 	RegionApSouthEast1: "gateway.aws-ap-southeast-1.bytehouse.cloud:19000",
+	RegionUsEast1:      "gateway.aws-us-east-1.bytehouse.cloud:19000",
 }
 
 func regionNotFound(region string) error {
